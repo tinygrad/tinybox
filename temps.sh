408e989154c6edf85fe2ed85ea42cc18f9ae0e08
George Hotz <geohot@gmail.com>
2023-11-27 20:10:36 -0800

    temps

--- temps.sh
+++ temps.sh
@@ -1,5 +1,12 @@
 #!/bin/bash
+RED='\033[0;31m'
+NC='\033[0m'
+echo -e "${RED}gpus${NC}"
+sudo sensors | grep edge
+echo -e "${RED}disks${NC}"
 nvme smart-log /dev/nvme0 -H | grep temperature
 nvme smart-log /dev/nvme1 -H | grep temperature
 nvme smart-log /dev/nvme2 -H | grep temperature
 nvme smart-log /dev/nvme3 -H | grep temperature
+echo -e "${RED}network${NC}"
+mget_temp -d /dev/mst/mt4123_pciconf0
