b386c4ed1660957c5dc53096636d20da69a7a728
George Hotz <geohot@gmail.com>
2024-01-22 23:41:14 +0000

    new new user

--- new_user.sh
+++ new_user.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
-adduser $1
+adduser $1 --disabled-password
 usermod -a -G sudo $1
 usermod -a -G render $1
 usermod -a -G video $1
-su $1
+su - $1 -c "mkdir /home/$1/.ssh"
+su - $1 -c "echo $2 > /home/$1/.ssh/authorized_keys"
