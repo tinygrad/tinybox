83ad9b63214dc368febb44f31ea91198c0423735
George Hotz <geohot@gmail.com>
2024-03-01 11:17:24 +0000

    update create raid

--- temps.sh
+++ temps.sh
@@ -9,4 +9,6 @@ nvme smart-log /dev/nvme1 -H | grep temperature
 nvme smart-log /dev/nvme2 -H | grep temperature
 nvme smart-log /dev/nvme3 -H | grep temperature
 echo -e "${RED}network${NC}"
-mget_temp -d /dev/mst/mt4123_pciconf0
+# https://network.nvidia.com/products/adapter-software/firmware-tools/
+mget_temp -d /dev/mst/mt4121_pciconf0
+#mget_temp -d /dev/mst/mt4123_pciconf0
