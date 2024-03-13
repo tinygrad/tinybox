e8259a166f25267e4f5d934a0afc06cc5950eeb1
George Hotz <geohot@gmail.com>
2024-03-13 20:09:07 +0000

    create raid dir

--- create_raid.sh
+++ create_raid.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 mdadm --create --verbose /dev/md0 --level=0 --raid-devices=4 /dev/nvme0n1 /dev/nvme1n1 /dev/nvme2n1 /dev/nvme3n1
 mkfs.ext4 /dev/md0
+mkdir -p /raid
 mount /dev/md0 /raid/
 mkdir -p /raid/weights
 chown tiny:tiny /raid/weights
